8eb330661b75555dcbc5611c037b6e5edccc2b29
Vinícius <vncssj@gmail.com>
2021-07-05 21:06:18 -0300

    Links de navegação

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
     <h2>Home</h2>
+    <p>
+      <a href="/form">Envio de Planilha</a>
+    </p>
+    <p>
+      <a href="http://localhost:8000/csv/animais"
+        >Download de Planilha de Animais</a
+      >
+    </p>
+    <p>
+      <a href="http://localhost:8000/csv/clientes"
+        >Download de Planilha de Cliente</a
+      >
+    </p>
   </div>
 </template>
 
